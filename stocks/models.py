5c4ab7059c324c7df4b6c0d5f3c05c417ecb4fce
Manoj <pmontu@gmail.com>
2015-03-09 16:13:13 +0530

    models.Book with 1 row

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	pub_date = models.DateField()
+	title = models.CharField(max_length=200)
+	field_of_study = models.CharField(max_length=1, 
+		choices = (
+			("C","C"),
+			("J","Java"),
+			("H","PHP"),
+			("P","Python")
+			))
